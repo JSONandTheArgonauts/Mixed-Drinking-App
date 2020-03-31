957d600bc10b089b381760311a7eba74fa4ef263
Zach012 <zachdillon012@gmail.com>
2020-03-31 12:53:09 -0400

    adding controller

---
+++ src/main/java/com/example/mixeddrinkapp/DrinkController.java
@@ -0,0 +1,69 @@
+package com.example.mixeddrinkapp;
+
+import java.util.Optional;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class DrinkController {
+
+	@Resource
+	private DrinkRepository drinkRepo;
+
+	@Resource
+	private Drink drinkId;
+	
+	@Resource
+	private MixerRepository mixerRepo;
+	
+	@Resource
+	private Mixer mixerId;
+
+	@RequestMapping("show-drink")
+	public String findOneDrink(@RequestParam(value = "id") long id, Model model) {
+		Optional<Drink> drink = drinkRepo.findById(id);
+		model.addAttribute("drinks", drink.get());
+		return ("drink");
+
+	}
+
+	@RequestMapping("show-drinks")
+	public String findAllDrinks(Model model) {
+		model.addAttribute("drinks", drinkRepo.findAll());
+		return ("drinks");
+
+	}
+
+	@RequestMapping("show-mixer")
+	public String findOneMixer(@RequestParam(value = "id") long mixerId, Model model) {
+		model.addAttribute("mixers", mixer.get());
+		return ("mixer");
+	}
+
+	@RequestMapping("show-mixers")
+	public String findAllMixers(Model model) {
+		model.addAttribute("mixers", mixerRepo.findAll());
+		return ("mixers");
+		
+	}
+
+	@RequestMapping("show-garnish")
+	public String findOneGarnish(@RequestParam( value = "id") long garnishId, Model model) {
+		Optional<Garnish> garnish = garnishRepo.findById(id);
+		model.addAttribute("garnishes", garnish.get());
+		return ("garnish");
+	}
+
+	@RequestMapping("show-garnishes")
+	public String findAllGarnishes(Model model) {
+		model.addAttribute("garnishes", garnishRepo.findAll());
+		return ("garnishes");
+		
+	}
+
+}
---
+++ src/test/java/com/example/mixeddrinkapp/DrinkControllerTest.java
@@ -0,0 +1,112 @@
+package com.example.mixeddrinkapp;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Optional;
+
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.ui.Model;
+
+public class DrinkControllerTest {
+	
+	@InjectMocks
+	private DrinkController underTest;
+	
+	@Mock
+	private Drink drink;
+	
+	@Mock
+	private Drink anotherDrink;
+	
+	@Mock
+	private DrinkRepository drinkRepo;
+	
+	@Mock
+	private Mixer mixer;
+	
+	@Mock
+	private Mixer anotherMixer;
+	
+	@Mock
+	private MixerRepository mixerRepo;
+	
+	@Mock
+	private Garnish garnish;
+	
+	@Mock
+	private Garnish anotherGarnish;
+	
+	@Mock
+	private GarnishRepository garnishRepo;
+	
+	@Mock
+	private Model model;
+	
+	@Before
+	public void setUp() {
+		MockitoAnnotations.initMocks(this);
+	}
+	
+	@Test
+	public void shouldAddSingleDrinkToModel() {
+		long drinkId = 1L;
+		when(drinkRepo.findById(drinkId)).thenReturn(Optional.of(drink));
+		
+		underTest.findOneDrink(drinkId, model);
+		verify(model).addAttribute("drinks", drink);
+	}
+	
+	@Test
+	public void shouldAddAllDrinksToModel() {
+		Collection<Drink> allDrinks = Arrays.asList(drink, anotherDrink);
+		when(drinkRepo.findAll()).thenReturn(allDrinks);
+		
+		underTest.findAllDrinks(model);
+		verify(model).addAttribute("drinks", allDrinks);
+	}
+	
+	@Test
+	public void shouldAddSingleMixerToModel() {
+		long mixerId = 1L;
+		when(mixerRepo.findById(mixerId)).thenReturn(Optional.of(mixer));
+		
+		underTest.findOneMixer(mixerId, model);
+		verify(model).addAttribute("mixers", mixer);
+	}
+	
+	@Test
+	public void shouldAddAllMixersToModel() {
+		Collection<Mixer> allMixers = Arrays.asList(mixer, anotherMixer);
+		when(mixerRepo.findAll()).thenReturn(allMixers);
+		
+		underTest.findAllMixers(model);
+		verify(model).addAttribute("mixers", allMixers);
+	}
+	
+	@Test
+	public void shouldAddSingleGarnishToModel() {
+		long garnishId = 1L;
+		when(garnishRepo.findById(garnishId)).thenReturn("garnishes", garnish);
+		
+		underTest.findOneGarnish(garnishId, model);
+		verify(model).addAttribute("garnishes", garnish);
+	}
+	
+	@Test
+	public void shouldAddAllGarnishesToModel() {
+		Collection<Garnish> allGarnishes = Arrays.asList(garnish, anotherGarnish);
+		when(garnishRepo.findAll()).thenReturn(allGarnishes);
+		
+		underTest.findAllGarnishes(model);
+		verify(model).addAttribute("garnishes", allGarnishes);
+	}
+
+}
